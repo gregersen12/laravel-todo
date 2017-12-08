72bb947e0616abcb2b9aae7f3cf7263272248e43
Martin Aarhus Gregersen <aarhus@dinero.dk>
2017-12-08 14:18:43 +0100

    Initialize project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<html lang="en">
+    <head>
+        <title>Martins Laravel todo</title>
+
+        <!-- CSS And JavaScript -->
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
